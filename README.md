eb57e0223caf140a7fb780cb323b9036218cb215
Yuchao Han <hanyc1992@gmail.com>
2021-09-22 00:03:05 +0800

    init commit with working version

---
+++ README.md
@@ -0,0 +1,9 @@
+# How to run app locally
+
+```
+python -m venv venv
+
+pip install -r requirements.txt
+
+python -m flask run
+```
---
+++ app.py
@@ -0,0 +1,43 @@
+"""
+This is a base webapp using Flask
+
+Tutorial: https://code.visualstudio.com/docs/python/tutorial-flask
+"""
+
+
+from datetime import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Replace the existing home function with the one below
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name=None):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
